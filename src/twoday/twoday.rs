08b7c953ea71f5dd6b80d4fefc774d292b9cb11e
fy_tang <1467827322@qq.com>
2024-09-27 10:31:12 +0800

    three day begin

--- src/twoday/twoday.rs
+++ src/twoday/twoday.rs
@@ -6,10 +6,64 @@ fn add_int_float(x:i32,y:f32) -> f32 {
     (x as f32) + y
 }
 
+fn copys(n:i32){
+    let s1 =String::from("fuck");
+    let s2 = s1.clone();
+    println!("{}",n)
+}
+
+fn takes_ownership(some_string :String) {
+    println!("{}",some_string)
+}
+
+fn borrow_fn(){
+    let x = 5;
+    let y = &x;
+    let s = String::from("fuck");
+    let s1 = &s;
+    println!("{}",*s1);
+    assert_eq!(s,*s1);
+    assert_eq!(x,*y);
+}
+
+fn calculate_length(s: &mut String) {
+    s.len();
+    s.insert_str(1, " you")
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
 #[cfg(test)]
 mod test{
-    use super::{add_int_float, hello_twoday};
+    use crate::twoday::twoday::{change, copys};
 
+    use super::{add_int_float, borrow_fn, calculate_length, hello_twoday, takes_ownership};
+
+    #[test]
+    fn test_calculate_length(){
+        let mut s = String::from("fuck");
+        calculate_length(&mut s);
+        println!("{}",s);
+        change(&mut s);
+        println!("{}",s);
+    }
+
+    #[test]
+    fn test_borrow_fn(){
+        borrow_fn();
+    }
+
+    #[test]
+    fn test_takes_ownership(){
+        let s = String::from("fuck");
+        takes_ownership(s);
+        // println!("{}",s)
+        let n = 5;
+        copys(n);
+        println!("{}",n)
+    }
     #[test]
     fn test_hello_twoday(){
         hello_twoday();
