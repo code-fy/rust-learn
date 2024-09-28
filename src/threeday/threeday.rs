eeed79142983e49bc27c20d60a0d6799e99c05fc
fy_tang <1467827322@qq.com>
2024-09-28 21:47:34 +0800

    to method and funcation

--- src/threeday/threeday.rs
+++ src/threeday/threeday.rs
@@ -23,14 +23,37 @@ fn nodangle() -> String {
 // nodangle 返回了内部函数的所有权给到调用者
 // 引用的作用域从创建开始一直持续到他最后一次使用的地方，这个跟变量的作用域有所不同，变量的作用域从创建持续到某一个花括号
 
+// 复合类型
+#[allow(unused_variables)]
+type File = String;
+fn open(f: &mut File) -> bool {
+    true
+}
+fn close(f:&mut File) -> bool{
+    true
+}
+#[allow(dead_code)]
+fn read(f:&mut File,save_to: &mut Vec<u8>) -> !{
+    unimplemented!()
+}
 
 
 #[cfg(test)]
 mod test{
-    use super::borrow_method;
+
+    use super::{borrow_method, close, open, File,read};
 
     #[test]
     fn test_borrow_method(){
         borrow_method();
+    }
+
+    #[test]
+    fn test_file_systeam(){
+        let mut f = File::from("fuck");
+        let op = open(&mut f);
+        let op = close(&mut f);
+        // read(&mut f,& mut vec![]);
+
     }
 }
