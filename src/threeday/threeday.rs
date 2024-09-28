0f640e813bce36413c637f0b18066d745da3d83a
fy_tang <1467827322@qq.com>
2024-09-28 21:47:48 +0800

    update

--- src/threeday/threeday.rs
+++ src/threeday/threeday.rs
@@ -24,7 +24,7 @@ fn nodangle() -> String {
 // 引用的作用域从创建开始一直持续到他最后一次使用的地方，这个跟变量的作用域有所不同，变量的作用域从创建持续到某一个花括号
 
 // 复合类型
-#[allow(unused_variables)]
+#[allow(unused_variables)] 
 type File = String;
 fn open(f: &mut File) -> bool {
     true
@@ -37,11 +37,44 @@ fn read(f:&mut File,save_to: &mut Vec<u8>) -> !{
     unimplemented!()
 }
 
+fn first_word(s: &String) -> &str {
+    &s[..1]
+}
+
+fn slice_model(l:&[i32]) ->&[i32]{
+    &l[0..3]
+}
+
+fn greetings(s: String) {
+    println!("{}",s)
+}
+
 
 #[cfg(test)]
 mod test{
 
-    use super::{borrow_method, close, open, File,read};
+    use super::{borrow_method, close, greetings, open, read, slice_model, File};
+
+    #[test]
+    fn test_greetings(){
+        let s = String::from("fuck");
+        greetings(s);
+    }
+
+    #[test]
+    fn test_slice_model(){
+        let mut l = [1,2,3,4,5,6];
+        let r = slice_model(&l);
+        println!("{:?},{:?}",r,l)
+    }
+
+    #[test]
+    fn test_slice(){
+        let s = String::from("value");
+        let S = &s[1..2];
+
+        
+    }
 
     #[test]
     fn test_borrow_method(){
